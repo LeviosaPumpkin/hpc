2c7a3830f478e18d5d7fdd721e8b0b6ba3df2475
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2018-10-10 22:02:39 +0300

    initial commit

---
+++ 6/main.cpp
@@ -0,0 +1,33 @@
+/*Дана последовательность натуральных чисел {a0...an–1}. 
+Создать OpenMP-приложение для поиска суммы ∑ai, 
+где ai – простые числа. */
+#include <omp.h>
+#include <iostream>
+#include <vector>
+
+void print(const std::vector<int> &v)
+{
+	for(int i=0;i<v.size();++i) std::cout << v[i] << " ";
+	std::cout<<std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	const int n=100;
+	std::vector<int> a(n);
+	int sum=0;
+	for (int i=0; i<n; ++i) a[i]=i;
+	std::cout<<n<<std::endl;
+	int j;
+	for(int i=0; i<n; ++i)
+	{
+		if(1==a[i] || 0==a[i]) continue;
+		for(j=2; j<=(a[i]/j);++j)
+		{
+			if(!(a[i]%j)) break;
+		}
+		if(j>(a[i]/j)) sum+=a[i];
+	}
+	std::cout<<sum<<std::endl;
+	return 0;
+}
