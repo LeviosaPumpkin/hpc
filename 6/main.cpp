46e6f8d8b6d8fe26af7d2efc586b9c764119372b
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2018-10-11 09:58:05 +0300

    added max count of threads

--- 6/main.cpp
+++ 6/main.cpp
@@ -13,16 +13,19 @@ void print(const std::vector<int> &v)
 
 int main(int argc, char **argv)
 {
+	
 	const int n=100;
 	std::vector<int> a(n);
 	int sum=0;
 	for (int i=0; i<n; ++i) a[i]=i;
 	std::cout<<n<<std::endl;
-	int j;
+	int j=2;
+	std::cout<<"Max threads: "<< omp_get_max_threads()<<std::endl;
+	#pragma omp parallel for
 	for(int i=0; i<n; ++i)
 	{
 		if(1==a[i] || 0==a[i]) continue;
-		for(j=2; j<=(a[i]/j);++j)
+		for(;j<=(a[i]/j);++j)
 		{
 			if(!(a[i]%j)) break;
 		}
