6beb8db369be867f4648f3e81f2328284fee1569
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2018-10-11 10:42:32 +0300

    made parallel right

--- 6/main.cpp
+++ 6/main.cpp
@@ -14,14 +14,17 @@ void print(const std::vector<int> &v)
 int main(int argc, char **argv)
 {
 	
-	const int n=100;
+	const int n=100;//sum=4665
 	std::vector<int> a(n);
 	int sum=0;
 	for (int i=0; i<n; ++i) a[i]=i;
 	std::cout<<n<<std::endl;
 	int j=2;
-	std::cout<<"Max threads: "<< omp_get_max_threads()<<std::endl;
-	#pragma omp parallel for
+	int max_threads = omp_get_max_threads();
+	std::cout<<"Max threads: "<< max_threads <<std::endl;
+	if (max_threads<4) omp_set_num_threads(max_threads);
+	else omp_set_num_threads(4);          
+	#pragma omp parallel for schedule(static,250) shared(a) private(i) reduction(+:sum)
 	for(int i=0; i<n; ++i)
 	{
 		if(1==a[i] || 0==a[i]) continue;
