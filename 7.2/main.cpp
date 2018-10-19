4f9c30d7f203b6f75cd7537d48e5fa3c3bb12862
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2018-10-19 13:22:48 +0300

    omp 7.2

--- 7.2/main.cpp
+++ 7.2/main.cpp
@@ -7,11 +7,18 @@ int main(int argc, char **argv)
 	int n=4;
 	int a = 2;
 	int b=3;
-	
-	for(int i=1,j=0; i<7; i+=2,++j)
+	int j=0;
+	int i=0;
+	omp_set_num_threads(n);
+	#pragma omp parallel for schedule(static) reduction(+:j)
+	for(i=1; i<7; i+=2) 
 	{
-		std::cout<<"C"<<i<<"*"<<a<<"^"<<n-j<<"*x^"<<n-j<<"*"<<b<<"^"<<j<<"*y^"<<j<<
-			"+C"<<i+1<<"*"<<b<<"^"<<n-j<<"*y^"<<n-j<<"*"<<a<<"^"<<j<<"*x^"<<j<<"+\n";
+		#pragma omp critical
+		{
+			std::cout<<"C"<<i<<"*"<<a<<"^"<<n-j<<"*x^"<<n-j<<"*"<<b<<"^"<<j<<"*y^"<<j<<
+				"+C"<<i+1<<"*"<<b<<"^"<<n-j<<"*y^"<<n-j<<"*"<<a<<"^"<<j<<"*x^"<<j<<"+\n";
+		}
+		++j;
 	}
 	std::cout<<"+K\n";
 	return 0;
